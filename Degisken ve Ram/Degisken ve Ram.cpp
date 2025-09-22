a0beb9fc9b7a369d3a9161b6878e254c10547337
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 20:52:30 +0300

    Ekle: C++ DEğişkenler ve Ram örneği ve yapılandırma dosyaları

---
+++ Degisken ve Ram/Degisken ve Ram.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+
+	//int a(10);
+
+	//std::cout << &a; //BELLEK ADRESİNİ VERİR
+
+	int x{ 20 };
+
+	int* xPtr = &x;
+	//int * xPtr= &x;
+	//int *xPtr = &x;
+
+	std::cout << xPtr;
+
+	int b(10);
+
+	std::cout << *&b;
+}
