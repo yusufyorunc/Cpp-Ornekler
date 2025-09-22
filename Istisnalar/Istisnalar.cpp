2b3741f310e84f4f671e151cea41dfd8226ae264
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 20:58:58 +0300

    Ekle: C++'da istisnalara örnekler ve yapılandırma dosyaları

---
+++ Istisnalar/Istisnalar.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+class Hata {
+public:
+	Hata(const std::string msj) : mesaj(msj) {
+	};
+
+	std::string getMesaj() {
+		return this->mesaj;
+	}
+private:
+	std::string mesaj;
+
+};
+
+
+
+double bolme(int a, int b) {
+	try
+	{
+		if (b < 1)
+		{
+			throw Hata("Hata olustu\n");
+
+		}
+		return a / b;
+	}
+	catch (int)
+	{
+		std::cout << "Sayi sifira bolunemez";
+	}
+	catch (Hata err) {
+		std::cout << err.getMesaj();
+	};
+};
+
+
+int main()
+{
+	std::cout << bolme(10, 0);
+}
