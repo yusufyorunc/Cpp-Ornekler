02b8bcc473093560465afe3a03e49affa0b2c5ab
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 21:00:55 +0300

    Ekle: C++ Kapsülleme için örnekler ve yapılandırma dosyaları

---
+++ Kapsulleme/Kapsulleme.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+class Payment
+{
+public:
+	std::string paraBirimi;
+
+	void setTutar(double t) {
+		if (t > 0) {
+			tutar = t;
+		}
+		else {
+			tutar = 0.1;
+		}
+	};
+
+	void pay() {
+		std::cout << tutar << " " << paraBirimi << " odendi." << std::endl;
+	};
+
+private:
+	float tutar;
+};
+
+
+int main()
+{
+	Payment pay;
+	pay.paraBirimi = "TL";
+	pay.setTutar(50.70);
+	pay.pay();
+}
