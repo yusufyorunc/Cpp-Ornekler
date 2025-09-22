addc1d872f7dff8aca5bc324fa0aa7e4565c852c
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 20:55:57 +0300

    Ekle: Fonksiyon CallBack için temel fonksiyonlar ve yapılandırma dosyaları

---
+++ Fonksiyon CallBack/Fonksiyon CallBack.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+int toplama(int a, int b) {
+	return a + b;
+}
+
+
+int callback(int(*toplamaIslemi)(int, int)) {
+	int a = toplamaIslemi(10, 10);
+	return a;
+}
+
+using fint = int(*)(int, int);
+
+int callback2(fint ftopla) {
+	int x = ftopla(20, 20);
+	return x;
+}
+
+int main()
+{
+
+	int b = callback(toplama);
+	int c = callback2(toplama);
+
+	std::cout << b << '\n' << c;
+}
