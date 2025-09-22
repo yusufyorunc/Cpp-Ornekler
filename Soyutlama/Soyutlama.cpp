8a30d772c66bd9622c62a7101d026daef7d3b9c9
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 21:04:03 +0300

    Ekle: C++ Soyutlamaya yönelik örnekler ve yapılandırma dosyaları

---
+++ Soyutlama/Soyutlama.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Hayvan {
+public:
+	virtual void sesCikar() = 0;
+};
+
+class Kopek : public Hayvan {
+public:
+	void sesCikar() {
+		std::cout << "Hav hav!" << std::endl;
+	};
+};
+
+class Kedi : public Hayvan {
+public:
+	void sesCikar() {
+		std::cout << "Miuv Miuv!" << std::endl;
+	};
+};
+
+class Ordek : public Hayvan {
+public:
+	void sesCikar() {
+		std::cout << "Vak Vak!" << std::endl;
+	};
+};
+
+class Kus : public Hayvan {
+public:
+	void sesCikar() {
+		std::cout << "Cik Cik!" << std::endl;
+	};
+};
+
+void tumSesleriCikar(Hayvan* hayvan) {
+	hayvan->sesCikar();
+}
+
+int main()
+{
+	Kopek kopek;
+	Kedi kedi;
+	Ordek ordek;
+	Kus kus;
+
+	tumSesleriCikar(&kus);
+}
