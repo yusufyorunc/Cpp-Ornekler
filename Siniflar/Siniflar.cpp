d5c5dcbc500edab1e2ee729f1ed574dd3a73839e
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 21:03:44 +0300

    Ekle: C++ Sınıflar(Class) için örnekler ve yapılandırma dosyaları

---
+++ Siniflar/Siniflar.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+class Hesapla {
+public:
+	int genislik;
+	int yukseklik;
+	int alan;
+
+	void giris() {
+		std::cout << "Genislik gir: ";
+		std::cin >> genislik;
+
+		std::cout << "Yukseklik gir: ";
+		std::cin >> yukseklik;
+	};
+
+	void hesapla() {
+		alan = genislik * yukseklik;
+		std::cout << "Alan: " << alan << std::endl;
+	};
+};
+
+//class hacim {
+//public:
+//	int uzunluk;
+//	int genislik;
+//	int yukseklik;
+//	int hacim;
+//
+//	void veriler() {
+//		std::cout << "Hacim hesaplama " << std::endl;
+//		std::cout << "Uzunluk gir: ";
+//		std::cin >> uzunluk;
+//
+//		std::cout << "Genislik gir: ";
+//		std::cin >> genislik;
+//
+//		std::cout << "Yukseklik gir: ";
+//		std::cin >> yukseklik;
+//
+//	};
+//	void hacimhesapla() {
+//		hacim = uzunluk * genislik * yukseklik;
+//		std::cout << "Hacim: " << hacim << std::endl;
+//	}
+//};
+
+int main()
+{
+	/*hacim kutu;
+	kutu.veriler();
+	kutu.hacimhesapla();*/
+
+
+	Hesapla dikdortgen;
+	dikdortgen.giris();
+	dikdortgen.hesapla();
+	return 0;
+}
