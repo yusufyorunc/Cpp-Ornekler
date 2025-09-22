a396563e06acd0be1947d9a147c85cc8f72a0689
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 21:04:21 +0300

    Ekle: C++ Switch kullanımı için örnekler ve yapılandırma dosyaları

---
+++ Switch Kullanimi/Switch Kullanimi.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+	int sayi1(0), sayi2(0);
+	char islem;
+
+	std::cout << "birinci sayiyi girin: ";
+	std::cin >> sayi1;
+	std::cout << "ikinici sayiyi girin: ";
+	std::cin >> sayi2;
+	std::cout << "yapmak istediginiz islemi girin(+, -, *, /): ";
+	std::cin >> islem;
+
+	switch (islem)
+	{
+	case '+':
+		std::cout << "sonuc: " << sayi1 + sayi2 << std::endl;
+		break;
+	case '-':
+		std::cout << "sonuc: " << sayi1 - sayi2 << std::endl;
+		break;
+	case '*':
+		std::cout << "sonuc: " << sayi1 * sayi2 << std::endl;
+		break;
+	case '/':
+		std::cout << "sonuc: " << sayi1 / sayi2 << std::endl;
+		break;
+	default:
+		std::cout << "gecersiz islem" << std::endl;
+		break;
+	}
+
+	std::cout << "\n Bitti";
+}
+
