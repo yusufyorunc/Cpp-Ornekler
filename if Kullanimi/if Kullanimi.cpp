4cfc54546229e167ab9de658afd7f2a8c901337a
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 20:58:24 +0300

    Ekle: C++ if kullanımı için örnekler ve yapılandırma dosyaları

---
+++ if Kullanimi/if Kullanimi.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int main()
+{
+
+	int sayi1(0), sayi2(0);
+	char islem;
+
+	std::cout << "birinci sayiyi girin: ";
+	std::cin >> sayi1;
+	std::cout << "ikinici sayiyi girin: ";
+	std::cin >> sayi2;
+	std::cout << "yapmak istediginiz islemi girin(+, -, *, /): ";
+	std::cin >> islem;
+
+	if (islem == '+')
+	{
+		int toplama = sayi1 + sayi2;
+		std::cout << "sonuc: " << toplama << std::endl;
+	}
+	else if (islem == '-')
+	{
+		int cıkarma = sayi1 - sayi2;
+		std::cout << "sonuc: " << cıkarma << std::endl;
+	}
+	else if (islem == '*')
+	{
+		int carpma = sayi1 * sayi2;
+		std::cout << "sonuc: " << carpma << std::endl;
+	}
+	else if (islem == '/')
+	{
+		int bolme = sayi1 / sayi2;
+		std::cout << "sonuc: " << bolme << std::endl;
+	}
+	else
+	{
+		std::cout << " islem yapilamadi" << std::endl;
+	}
+}
