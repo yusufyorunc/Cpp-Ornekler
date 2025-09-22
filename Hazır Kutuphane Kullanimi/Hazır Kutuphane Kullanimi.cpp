3d16037d7127c7f657352637ccf0c41dcdaafce3
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 20:57:27 +0300

    Ekle: C++ Kütüphane kullanıma örnekler ve yapılandırma dosyaları

---
+++ Hazır Kutuphane Kullanimi/Hazır Kutuphane Kullanimi.cpp
@@ -0,0 +1,22 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string* yaz = new string;
+	ofstream* dosya = new ofstream("deneme.txt");
+
+	if (dosya->is_open())
+	{
+		cin >> *yaz;
+		*dosya << *yaz;
+		dosya->close();
+	}
+	else
+	{
+		cout << "Dosya acilamadi!" << endl;
+	}
+}
