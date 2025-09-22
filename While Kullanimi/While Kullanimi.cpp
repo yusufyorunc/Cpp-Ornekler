07aadfc3a42a492857864bc34839be04129e7c77
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 21:04:36 +0300

    Ekle: C++ While kullanımı için örnekler ve yapılandırma dosyaları

---
+++ While Kullanimi/While Kullanimi.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+int main()
+{
+	while (true)
+	{
+		int sayi1(0), sayi2(0);
+		char islem;
+
+		std::cout << "birinci sayiyi girin: ";
+		std::cin >> sayi1;
+		std::cout << "ikinici sayiyi girin: ";
+		std::cin >> sayi2;
+		std::cout << "yapmak istediginiz islemi girin(+, -, *, /, q (cikis)): ";
+		std::cin >> islem;
+
+		switch (islem)
+		{
+		case '+':
+			std::cout << "sonuc: " << sayi1 + sayi2 << std::endl;
+			break;
+		case '-':
+			std::cout << "sonuc: " << sayi1 - sayi2 << std::endl;
+			break;
+		case '*':
+			std::cout << "sonuc: " << sayi1 * sayi2 << std::endl;
+			break;
+		case '/':
+			std::cout << "sonuc: " << sayi1 / sayi2 << std::endl;
+			break;
+		case 'q':
+			std::cout << "cikis basariyla yapildi";
+			return 0;
+			break;
+		default:
+			std::cout << "gecersiz islem" << std::endl;
+			break;
+		}
+
+	}
+}
