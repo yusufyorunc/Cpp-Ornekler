97970a041b160ca5e50b0cb548d82662e4987f28
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 21:02:28 +0300

    Ekle: C++ Operatörler için örnekler ve yapılandırma dosyaları

---
+++ Operatorler/Operatorler.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+
+	int a = 10;
+	int b = 5;
+
+	int sonuc = a / b;
+	std::cout << sonuc;
+
+	int x, y;
+	x = 5;
+	y = 10;
+
+	x += y;
+	std::cout << x;
+
+	int a{ 21 }, b{ 20 };
+	std::cout << (a <= b) << std::endl;
+}
