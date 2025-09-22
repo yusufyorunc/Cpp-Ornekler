8f86d3eb1d1b1475293987c322a105e5eccee510
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-22 20:59:26 +0300

    Ekle: C++ iç içe dizilere örnekler ve yapılandırma dosyaları

---
+++ İc İce Diziler/İc İce Diziler.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iterator>
+
+int main()
+{
+	int dizi[1][1][1][1]{
+		{
+			{
+				{
+					{
+						123
+					}
+				}
+			}
+		}
+	};
+	std::cout << dizi[0][0][0][0];
+
+	int dizi2[2][2]{
+		{
+			1, 2
+		},
+		{
+			3, 4
+		}
+	};
+	std::cout << "\n" << dizi2[1][1];
+	std::cout << "\n" << std::size(dizi[0]);
+}
